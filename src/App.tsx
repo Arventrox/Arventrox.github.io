79de21c8d3c4e59e4c903158393cbe5e7f2750a2
Stiliyan Ivanov <stiliqnivanov124@gmail.com>
2022-12-04 21:17:18 +0200

    added linter

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import "./App.css";
-import RockPaperScissors from "./components/games/RockPaperScissors";
+import './App.css';
+import RockPaperScissors from './components/games/RockPaperScissors';
+import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <RockPaperScissors />
-    </div>
-  );
+	return (
+		<div className='App'>
+			<RockPaperScissors />
+		</div>
+	);
 }
 
 export default App;
--- src/components/games/RockPaperScissors.tsx
+++ src/components/games/RockPaperScissors.tsx
@@ -1,90 +1,93 @@
-import style from "./RockPaperScissors.module.css";
-import { useEffect, useState } from "react";
+import style from './RockPaperScissors.module.css';
+import React, {useEffect, useState} from 'react';
 const RockPaperScissors = () => {
-  const [computerResult, setComputerResult] = useState("");
-  const [playerResult, setPlayerResult] = useState("");
-  const [winner, setWinner] = useState("");
+	const [computerResult, setComputerResult] = useState('');
+	const [playerResult, setPlayerResult] = useState('');
+	const [winner, setWinner] = useState('');
 
-  const rock = "ROCK";
-  const scissors = "SCISSORS";
-  const paper = "PAPER";
-  const player = "PLAYER";
-  const computer = "COMPUTER";
+	const rock = 'ROCK';
+	const scissors = 'SCISSORS';
+	const paper = 'PAPER';
+	const player = 'PLAYER';
+	const computer = 'COMPUTER';
 
-  // Computer Chooses
+	// Computer Chooses
 
-  const computerChosen = () => {
-    const random = Math.random();
-    if (random < 0.33) {
-      setComputerResult(rock);
-    }
-    if (random > 0.33 && random < 0.66) {
-      setComputerResult(paper);
-    }
-    if (random > 0.66) {
-      setComputerResult(scissors);
-    }
-  };
+	const computerChosen = () => {
+		const random = Math.random();
 
-  //Player Chooses
+		if (random < 0.33) {
+			setComputerResult(rock);
+		}
 
-  const chosenHandler = (playerInput: string) => {
-    setPlayerResult(playerInput);
-    computerChosen();
-  };
+		if (random > 0.33 && random < 0.66) {
+			setComputerResult(paper);
+		}
 
-  //Winner is Chosen
-  useEffect(() => {
-    // eslint-disable-next-line default-case
-    switch (playerResult + computerResult) {
-      case "SCISSORSPAPER":
-      case "ROCKSCISSORS":
-      case "PAPERROCK":
-        setWinner(player);
-        break;
-      case "PAPERSCISSORS":
-      case "SCISSORSROCK":
-      case "ROCKPAPER":
-        setWinner(computer);
-        break;
-      case "ROCKROCK":
-      case "SCISSORSSCISSORS":
-      case "PAPERPAPER":
-        setWinner("DRAW");
-        break;
-    }
-  }, [computerResult, playerResult]);
+		if (random > 0.66) {
+			setComputerResult(scissors);
+		}
+	};
 
-  return (
-    <div>
-      <div className={style.container}>
-        <h1>Choose one:</h1>
-        <button
-          onClick={() => {
-            chosenHandler(rock);
-          }}>
-          ROCK
-        </button>
-        <button
-          onClick={() => {
-            chosenHandler(paper);
-          }}>
-          PAPER
-        </button>
-        <button
-          onClick={() => {
-            chosenHandler(scissors);
-          }}>
-          SCISSORS
-        </button>
-      </div>
-      <div>
-        <p>The Player Chose: {playerResult}</p>
-        <p>The Computer Chose: {computerResult} </p>
-        <p>The Winner is the :{winner}</p>
-      </div>
-    </div>
-  );
+	// Player Chooses
+
+	const chosenHandler = (playerInput: string) => {
+		setPlayerResult(playerInput);
+		computerChosen();
+	};
+
+	// Winner is Chosen
+	useEffect(() => {
+		// eslint-disable-next-line default-case
+		switch (playerResult + computerResult) {
+			case 'SCISSORSPAPER':
+			case 'ROCKSCISSORS':
+			case 'PAPERROCK':
+				setWinner(player);
+				break;
+			case 'PAPERSCISSORS':
+			case 'SCISSORSROCK':
+			case 'ROCKPAPER':
+				setWinner(computer);
+				break;
+			case 'ROCKROCK':
+			case 'SCISSORSSCISSORS':
+			case 'PAPERPAPER':
+				setWinner('DRAW');
+				break;
+		}
+	}, [computerResult, playerResult]);
+
+	return (
+		<div>
+			<div className={style.container}>
+				<h1>Choose one:</h1>
+				<button
+					onClick={() => {
+						chosenHandler(rock);
+					}}>
+					ROCK
+				</button>
+				<button
+					onClick={() => {
+						chosenHandler(paper);
+					}}>
+					PAPER
+				</button>
+				<button
+					onClick={() => {
+						chosenHandler(scissors);
+					}}>
+					SCISSORS
+				</button>
+			</div>
+			<div>
+				<p>The Player Chose: {playerResult}</p>
+				<p>The Computer Chose: {computerResult} </p>
+				<p>The Winner is the :{winner}</p>
+			</div>
+		</div>
+	);
 };
 
 export default RockPaperScissors;
