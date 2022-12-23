23f295c17235b2a91120f2f45abca621f05dabfe
Stiliyan Ivanov <stiliqnivanov124@gmail.com>
2022-12-23 22:52:24 +0200

    file structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import './App.scss';
-// import RockPaperScissors from './components/games/RockPaperScissors/RockPaperScissors';
 import React from 'react';
-import LegueRandomizer from './components/games/lolrandomizerr/LegueRandomizer';
+import LegueRandomizer from './components/LegueRandomizer';
 
 function App() {
   return (
     <div className='App'>
-      {/* <RockPaperScissors /> */}
       <LegueRandomizer />
     </div>
   );
